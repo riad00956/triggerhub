94eedf7245f9a64daea5725fe93c531594da9694
riad00956 <afrozshahana24@gmail.com>
2026-02-02 16:03:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from bot.instance import bot
 from core.scheduler import start_scheduler
 from core.restore import restore_jobs
 
-restore_jobs()
-start_scheduler()
-bot.infinity_polling()
+if __name__ == "__main__":
+    # Restore jobs from DB
+    restore_jobs()
+    
+    # Start APScheduler
+    start_scheduler()
+    
+    # Start bot polling (infinite)
+    bot.infinity_polling()
